902cd2209b1c231853ed16ffb0fbdcf1280027c5
Victor Graffard <victor.graffard@gmail.com>
2018-03-12 07:37:29 +0100

    Initiating app. skeleton, warnings left

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            // Some stuff here
+        };
+    }
+
+    render () {
+        return (
+        <div className="col-md-12">
+            Call the other components from here.
+        </div>
+        );
+    }
+}
+
+export default App;
